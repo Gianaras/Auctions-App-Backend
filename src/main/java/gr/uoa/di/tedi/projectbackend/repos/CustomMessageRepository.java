418ffd404692ec716b69d6f022cfda31ead9e628
Anargyros Argyros <sdi1900014@di.uoa.gr>
2022-06-08 20:07:33 +0300

    more to message controller

--- src/main/java/gr/uoa/di/tedi/projectbackend/repos/CustomMessageRepository.java
+++ src/main/java/gr/uoa/di/tedi/projectbackend/repos/CustomMessageRepository.java
@@ -2,12 +2,13 @@ package gr.uoa.di.tedi.projectbackend.repos;
 
 
 import gr.uoa.di.tedi.projectbackend.model.Message;
+import gr.uoa.di.tedi.projectbackend.model.MessageElement;
 import gr.uoa.di.tedi.projectbackend.model.User;
 
 import java.util.List;
 
 public interface CustomMessageRepository {
 
-    List<Message> getUserReceived(String username);
+    List<MessageElement> getUserReceived(String username);
     List<User> getRelevantUsers(String username);
 }
